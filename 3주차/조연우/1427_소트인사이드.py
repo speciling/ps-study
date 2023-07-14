fdd2ed298dd9a8b8169ee6bf271646383a27fe62
u_u <104762754+cathye0@users.noreply.github.com>
2023-07-14 20:24:37 +0900

    Add files via upload

---
+++ 3주차/조연우/1427_소트인사이드.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# 문자로 받아서 내림차순 정렬
+num = sorted(list(input()), reverse=True)
+for i in range(len(num)):
+    print(num[i], end='')
+
+
+# In[ ]:
+
+
+
+
