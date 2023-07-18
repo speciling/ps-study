656358000becad928497e24be05fcf37f4a8adcb
Kangboom <jkl6147@naver.com>
2023-07-18 15:39:22 +0900

    4week assigment

---
+++ 4주차/임강범/2775_부녀회장이 될테야.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for _ in range(t):
+    k = int(input())
+    n = int(input())
+    dp = [[j+1 if i == 0 else 0 for j in range(n)]for i in range(k)]
+    
+    for i in range(1,k):
+        for j in range(n):
+            for m in range(j+1):
+                dp[i][j] += dp[i-1][m]
+    print(sum([ dp[k-1][j] for j in range(n)]))
