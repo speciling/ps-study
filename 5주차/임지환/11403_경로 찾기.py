c212dd691ca2aa6a870312708db83d3ce922b229
dlawlghks <114456400+dlawlghks@users.noreply.github.com>
2023-07-28 19:31:39 +0900

    실버1/경로찾기/30분

---
+++ 5주차/임지환/11403_경로 찾기.py
@@ -0,0 +1,14 @@
+N = int(input())
+graph = []
+for _ in range(N):
+    graph.append(list(map(int, input().split())))
+
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if graph[i][k] == 1 and graph[k][j] == 1:
+                graph[i][j] = 1
+
+for i in graph:
+    print(*i)
