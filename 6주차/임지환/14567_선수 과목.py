002d44ce9d8329421cff37e92b64170072a96f24
dlawlghks <114456400+dlawlghks@users.noreply.github.com>
2023-08-04 15:20:44 +0900

    골드5/선수 과목/20분

---
+++ 6주차/임지환/14567_선수 과목.py
@@ -0,0 +1,28 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+n, m = map(int, input().split())
+q = deque()
+indegree = [0] * (n + 1)
+result = []
+graph = [[] for i in range(n + 1)]
+cnt = [0] * (n + 1)
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+for i in range(1, n + 1):
+    if indegree[i] == 0:
+        q.append(i)
+        cnt[i] = 1
+
+while q:
+    now = q.popleft()
+    result.append(now)
+    for i in graph[now]:
+        indegree[i] -= 1
+        if indegree[i] == 0:
+            q.append(i)
+            cnt[i] = cnt[now] + 1
+print(*cnt[1:])
